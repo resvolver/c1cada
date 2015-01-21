a990e0bce5fa2cb0cee3e286785d17cf632eb7e1
WetSand <3301@test.co.uk>
2015-01-21 16:34:35 +0100

    error fixed

--- perl/combine.sh
+++ perl/combine.sh
@@ -5,7 +5,7 @@ cat ../translation/transcriptions/[34567].txt                                   
 cat ../translation/transcriptions/[89].txt ../translation/transcriptions/1[01234].txt         > page.8-14.txt
 cat ../translation/transcriptions/1[56789].txt ../translation/transcriptions/2[012].txt       > page.15-22.txt
 cat ../translation/transcriptions/2[3456].txt                                                 > page.23-26.txt
-cat ../translation/transcriptions/2[3456].txt ../translation/transcriptions/3[012].txt        > page.27-32.txt
+cat ../translation/transcriptions/2[789].txt  ../translation/transcriptions/3[012].txt        > page.27-32.txt
 cat ../translation/transcriptions/3[3456789].txt                                              > page.33-39.txt
 cat ../translation/transcriptions/4[0123456789].txt ../translation/transcriptions/5[0123].txt > page.40-53.txt
 cat ../translation/transcriptions/5[45].txt                                                   > page.54-55.txt
